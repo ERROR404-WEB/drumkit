e8cc37fd4208136cf847f3bf5dc713cb278e3a8d
ARJUN KUMAR <102915876+ERROR404-WEB@users.noreply.github.com>
2022-08-18 11:00:42 +0530

    version update

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+var loader=document.getElementById("preloader");
+window.addEventListener("load",function()
+{
+    loader.style.display="none";
+});
+
 
 var len=document.querySelectorAll('.drum').length;
 
